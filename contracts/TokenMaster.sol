f1c1c8608acfeea9b736f043b93ae1362fd9d90d
Sean Coates <sean.coates+fooyay@gmail.com>
2025-12-19 12:52:38 -0500

    add ability to list an occasion

--- contracts/TokenMaster.sol
+++ contracts/TokenMaster.sol
@@ -5,8 +5,44 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract TokenMaster is ERC721 {
     address public owner;
+    uint256 public totalOccasions = 0;
+
+    struct Occasion {
+        uint256 id;
+        string name;
+        uint256 cost;
+        uint256 tickets;
+        uint256 maxTickets;
+        string date;
+        string time;
+        string location;
+    }
+
+    mapping(uint256 => Occasion) occasions;
 
     constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {
         owner = msg.sender;
+    }
+
+    function list(
+        string memory _name,
+        uint256 _cost,
+        uint256 _maxTickets,
+        string memory _date,
+        string memory _time,
+        string memory _location
+    ) public {
+        totalOccasions++;
+
+        occasions[totalOccasions] = Occasion({
+            id: totalOccasions,
+            name: _name,
+            cost: _cost,
+            tickets: _maxTickets,
+            maxTickets: _maxTickets,
+            date: _date,
+            time: _time,
+            location: _location
+        });
     }
 }
--- test/TokenMaster.js
+++ test/TokenMaster.js
@@ -4,18 +4,34 @@ const NAME = "TokenMaster"
 const SYMBOL = "TM"
 
 describe("TokenMaster", () => {
+    let tokenMaster
+    let deployer, buyer
+
+    const OCCASION_NAME = "ETH Texas"
+    const OCCASION_COST = ethers.utils.parseEther("1")
+    const OCCASION_MAX_TICKETS = 100
+    const OCCASION_DATE = "Apr 27"
+    const OCCASION_TIME = "10:00 AM CST"
+    const OCCASION_LOCATION = "Austin, TX"
+
+    beforeEach(async () => {
+        [deployer, buyer] = await ethers.getSigners()
+
+        const TokenMaster = await ethers.getContractFactory("TokenMaster")
+        tokenMaster = await TokenMaster.deploy(NAME, SYMBOL)
+
+        const transaction  = await tokenMaster.connect(deployer).list(
+            OCCASION_NAME,
+            OCCASION_COST,
+            OCCASION_MAX_TICKETS,
+            OCCASION_DATE,
+            OCCASION_TIME,
+            OCCASION_LOCATION
+        )
+        await transaction.wait()
+    })
 
     describe("Deployment", () => {
-        let tokenMaster
-        let deployer, buyer
-
-        beforeEach(async () => {
-            [deployer, buyer] = await ethers.getSigners()
-
-            const TokenMaster = await ethers.getContractFactory("TokenMaster")
-            tokenMaster = await TokenMaster.deploy(NAME, SYMBOL)
-        })
-
         it("Sets the name", async () => {
             let name = await tokenMaster.name()
             expect(name).to.equal(NAME)
@@ -30,5 +46,14 @@ describe("TokenMaster", () => {
             let owner = await tokenMaster.owner()
             expect(owner).to.equal(deployer.address)
         })
+    })
+
+    describe("Occasions", () => {
+
+        it("Updates occasion count on creation", async () => {
+            const totalOccasions = await tokenMaster.totalOccasions()
+            expect(totalOccasions).to.equal(1)
+        })
+
     })
 })
