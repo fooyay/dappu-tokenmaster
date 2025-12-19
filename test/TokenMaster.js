81ccbaebf494fbd762c0cb599e56d421741e400c
Sean Coates <sean.coates+fooyay@gmail.com>
2025-12-19 12:13:38 -0500

    set up basic ERC721 and a simple test

--- test/TokenMaster.js
+++ test/TokenMaster.js
@@ -1,5 +1,26 @@
 const { expect } = require("chai")
 
+const NAME = "TokenMaster"
+const SYMBOL = "TM"
+
 describe("TokenMaster", () => {
 
+    describe("Deployment", () => {
+        let tokenMaster
+
+        beforeEach(async () => {
+            const TokenMaster = await ethers.getContractFactory("TokenMaster")
+            tokenMaster = await TokenMaster.deploy(NAME, SYMBOL)
+        })
+
+        it("Sets the name", async () => {
+            let name = await tokenMaster.name()
+            expect(name).to.equal(NAME)
+        })
+
+        it("Sets the symbol", async () => {
+            let symbol = await tokenMaster.symbol()
+            expect(symbol).to.equal(SYMBOL)
+        })
+    })
 })
