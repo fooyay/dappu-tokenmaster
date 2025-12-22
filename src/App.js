49029f8af3cf4d8b30b1a8bb2270583f2484289d
Sean Coates <sean.coates+fooyay@gmail.com>
2025-12-22 11:38:56 -0500

    add navigation with connect button

--- src/App.js
+++ src/App.js
@@ -18,10 +18,6 @@ function App() {
   const [account, setAccount] = useState(null)
 
   const loadBlchainData = async () => {
-    // fetch account
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-    setAccount(ethers.utils.getAddress(accounts[0]))
-
     // refresh account on change
     window.ethereum.on('accountsChanged', async () => {
       const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
@@ -37,8 +33,8 @@ function App() {
   return (
     <div>
       <header>
-
-        <h2 className="header__title"><strong>Welcome to Tokenmaster</strong></h2>
+        <Navigation account={account} setAccount={setAccount} />
+        <h2 className="header__title"><strong>Event</strong> Tickets</h2>
       </header>
 
     </div>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,32 @@
 import { ethers } from 'ethers'
 
 const Navigation = ({ account, setAccount }) => {
+  const connectHandler = async () => {
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    setAccount(ethers.utils.getAddress(accounts[0]))
+  }
 
   return (
     <nav>
+      <div className="nav__brand">
+        <h1>TokenMaster</h1>
+        <input className="nav__search" type="text" placeholder="Search events, tickets, venues..." />
 
+        <ul className='nav__links'>
+          <li><a href="/">Concerts</a></li>
+          <li><a href="/">Sports</a></li>
+          <li><a href="/">Arts & Theater</a></li>
+          <li><a href="/">More</a></li>
+        </ul>
+      </div>
+
+      {account ? (
+        <button className="nav__connect" type="button">
+          {account.slice(0, 6)}...{account.slice(38, 42)}
+        </button>
+      ) : (
+        <button className="nav__connect" onClick={connectHandler} type="button">Connect</button>
+      )}
     </nav>
   );
 }
